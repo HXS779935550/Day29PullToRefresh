40dd34cbcac2bb92c5122f9d86b4ab4ec061a256
HXS779935550 <779935550@qq.com>
2016-06-14 00:50:55 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/shinelon/day29pulltorefresh/MainActivity.java
+++ app/src/main/java/com/example/shinelon/day29pulltorefresh/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //777777
+        //888888
         initListData();
         initData();
         ptf.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
